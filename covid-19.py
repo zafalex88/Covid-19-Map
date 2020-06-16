324a3b52d4861161a4ae75185837feebaf8f490d
Alex Zafiris <zafiris.alex@yahoo.com>
2020-06-16 17:53:57 +0300

    minor updates

--- covid-19.py
+++ covid-19.py
@@ -8,10 +8,10 @@ country = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19
 
 # renaming the df column names to lowercase
 country.columns = map(str.lower, country.columns)
-
-# changing province/state to state and country/region to country
+#print(country.to_string())
+# changing column name from country/region to country
 country = country.rename(columns={'country_region': 'country'})
-country = country.drop([66, 115], axis=0)
+country = country.drop([68, 116], axis=0)
 
 # creating world map using Map class
 corona_map = folium.Map(location=[11,0], tiles="openstreetmap", zoom_start=2, max_zoom = 7, min_zoom = 2)
@@ -20,7 +20,7 @@ for i in range(0,len(country)):
     folium.Circle(
         location=[country.iloc[i]['lat'], country.iloc[i]['long_']],
         fill=True,
-        radius=(int((np.log(country.iloc[i,4]+1.00001)))+0.2)*15000,
+        radius=(int((np.log(country.iloc[i,4]+1.00001)))+0.2)*13000,
         color='red',
         fill_color='indigo',
         tooltip="{0} , {1} confirmed cases".format(country.iloc[i][0], int(country.iloc[i][4]))
@@ -28,10 +28,11 @@ for i in range(0,len(country)):
     folium.Circle(
         location=[country.iloc[i]['lat'], country.iloc[i]['long_']],
         fill=True,
-        radius=(int((np.log(country.iloc[i,5]+1.00001)))+0.2)*9000,
-        color='red',
+        radius=(int((np.log(country.iloc[i,5]+1.00001)))+0.2)*6000,
+        color='blue',
         fill_color='blue',
         tooltip="{0} , {1} deaths".format(country.iloc[i][0], int(country.iloc[i][5]))
     ).add_to(corona_map)
+   
     
 corona_map.save('./corona_map.html')
